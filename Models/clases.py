6a1890831f8d40e1e30926b9f3afd75a141b9854
atparodi10 <joatorres@uamv.edu.ni>
2025-06-09 23:26:49 -0600

    Clases presentes en el programa

---
+++ Models/clases.py
@@ -0,0 +1,17 @@
+class Cliente:
+    def __init__(self, nombre, cedula, telefono):
+        self.nombre = nombre
+        self.id = cedula
+        self.telefono = telefono
+        
+    def __str__(self):
+        return f"Cliente: {self.nombre}, Cédula: {self.id}, Telefono: {self.telefono}"
+
+class Prodcut:
+    def __init__(self, producto, precio, stock):    
+        self.producto = producto
+        self.precio = precio
+        self.stock = stock
+    
+    def __str__(self):
+        return f"Producto: {self.producto}, Precio: {self.precio: .2f}, In Stock: {self.stock}"
