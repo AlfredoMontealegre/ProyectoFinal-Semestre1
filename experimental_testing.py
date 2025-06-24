06d1e70f2ad61cdcf18e0dba4270269c19f49405
AlfredoMontealegre <montealfredo24@gmail.com>
2025-06-23 18:52:04 -0600

    (TESTING) Agregar Cantidades

--- experimental_testing.py
+++ experimental_testing.py
@@ -4,6 +4,16 @@ productos = ['Adaptador', 'Atornillador', 'Alicate', 'Ambientador', 'Clavo', 'Co
 
 precios = [350, 1100, 350, 180, 12.50, 48.50, 50, 3100, 170, 170, 180, 125, 300, 3400, 1300, 200, 1500, 1200, 500, 470]
 
+
+# Unidades y Cantidades
 # testing
 num = int(input("Ingrese un numero, porfavor: "))
-print(f"{productos[num - 1]} C${precios[num - 1]}")
+item = (productos[num - 1])
+precio_item = (precios[num - 1])
+print(f"Has elegido: {item} C${precio_item}")
+
+cant = int(input(f"Ingrese la cantidad de: {productos[num - 1]} a llevar: "))
+
+converter = int(precio_item)
+total = (cant * converter)
+print(f"Por la cantidad de {cant} de {item} el precio es: {total}.")
