7b96827410667f3f55810df750646e8376750c93
AlfredoMontealegre <montealfredo24@gmail.com>
2025-06-23 18:22:30 -0600

    Actualizacion Precios y Productos

--- tienda.py
+++ tienda.py
@@ -1,13 +1,22 @@
+import os
+
 # Prototipo de Venta para los inventarios
 
 productos = ['Adaptador', 'Atornillador', 'Alicate', 'Ambientador', 'Clavo', 'Conector LED', 'Cinta Transparente',
              'Escalera', 'Extension', 'Foco Led', 'Interruptor', 'Lima', 'Mangera', 'Motosierra',
              'Navaja Multiusos', 'Regadera', 'Taladro', 'Yeso', 'Tijeras', 'Ventilador']
 
+precios = [350, 1100, 350, 180, 12.50, 48.50, 50, 3100, 170, 170, 180, 125, 300, 3400, 1300, 200, 1500, 1200, 500, 470]
+
 # 0 - 19 (Valores) | 20 Productos
 # Los precios se alinearan de acuerdo a los productos.
 
+# Un comando para limpiar la pantalla, solo necesitas poner "limpiar_pantalla() en algo y lo hace automaticamente"
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def mostrar_menu():
+    limpiar_pantalla()
     print("""====> Bienvenido a la Tienda, que quisiera comprar?
       (Presiona el numero del producto para elegirlo)
 
@@ -41,4 +50,4 @@ def pedir_seleccion():
 mostrar_menu()
 opcion = pedir_seleccion()
 producto_seleccionado = productos[opcion - 1]
-print(f"Has seleccionado: {producto_seleccionado}")
+print(f"Has seleccionado: {producto_seleccionado} C${precios[opcion - 1]}")
